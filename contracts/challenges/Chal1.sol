39437622c1937894adc9dbb690fbf0f4697a1729
zeze-zeze <zezectf@gmail.com>
2022-04-30 12:57:34 +0800

    feat: add contract template

---
+++ contracts/challenges/Chal1.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.0;
+
+import "./CheckSolved.sol";
+
+contract Chal1 is CheckSolved {
+
+}
---
+++ contracts/tools/HitconFaucet.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.6/contracts/access/Ownable.sol";
+
+contract HitconFaucet is Ownable {
+
+}
