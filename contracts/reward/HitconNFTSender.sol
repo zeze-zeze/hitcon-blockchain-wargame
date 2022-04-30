dadc3ef2fa8ee45d3f4c5a3969a00794bcdc7c53
zeze-zeze <zezectf@gmail.com>
2022-04-30 21:21:06 +0800

    feat: HitconNFTSender template

---
+++ contracts/reward/HitconNFTSender.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.6/contracts/token/ERC1155/ERC1155.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.6/contracts/access/Ownable.sol";
+
+contract CheckSolved is Ownable, ERC1155 {
+
+}
