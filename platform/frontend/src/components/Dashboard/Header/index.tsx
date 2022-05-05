af269ee6dca64c2771c7e0677530edd8c6368ed9
wxrdnx <wxrdnx@protonmail.com>
2022-05-05 17:46:44 +0800

    finish top menu bar and sidebar

--- platform/frontend/src/components/Dashboard/Header/index.tsx
+++ platform/frontend/src/components/Dashboard/Header/index.tsx
@@ -1,40 +1,57 @@
 import { FC } from 'react';
-import { Box, Hidden, IconButton, Tooltip } from '@mui/material';
+import { Box, Hidden, IconButton, Tooltip, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import MenuTwoToneIcon from '@mui/icons-material/MenuTwoTone';
+import CloseTwoToneIcon from '@mui/icons-material/CloseTwoTone';
+import HeaderButtons from './Buttons';
 
-const HeaderWrapper = styled(Box)(
+const HeaderWrapperLg: FC = styled(Box)(
     ({ theme }) => ({
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        position: 'fixed',
+        top: 0,
+        left: theme.sidebar.width,
+        right: 0,
+        width: 'auto',
         height: theme.header.height,
         color: theme.header.textColor,
-        padding: theme.spacing(0, 2),
-        right: 0,
-        zIndex: 5,
+        padding: theme.spacing(3),
+        zIndex: -8,
         backgroundColor: theme.header.background,
         boxShadow: theme.header.boxShadow,
-        position: 'fixed',
-        justifyContent: 'space-between',
-        width: '100%',
-        [`@media (min-width: ${theme.breakpoints.values.lg}px`]: {
-            left: theme.sidebar.width,
-            width: 'auto',
-        }
+    })
+);
+
+const RightButtonsWrapperLg: FC = styled(Box)(
+    ({ theme }) => ({
+        display: 'flex',
+        flexWrap: 'nowrap',
+        justifyContent: 'space-around',
+        alignItems: 'center',
     })
 );
 
 const Header: FC = ({ sidebarToggled, toggleSidebar }) => {
     return (
-        <HeaderWrapper display="flex" alignItems="center">
-            <Box display="flex" alignItems="center">
-                <HeaderButtons />
-                <HeaderUserbox />
-                <Hidden lgUp>
+        <Hidden lgDown>
+            <HeaderWrapperLg>
+                <Typography variant="h3" component="h3">
+                    Dashboard
+                </Typography>
+                <RightButtonsWrapperLg>
+                    <HeaderButtons />
                     <Tooltip arrow title="Toggle Menu">
                         <IconButton color="primary" onClick={toggleSidebar}>
-                            {!sidebarToggle ? <MenuTwoToneIcon /> : <CloseTwoToneIcon />}
+                            {!sidebarToggled ? <MenuTwoToneIcon /> : <CloseTwoToneIcon />}
                         </IconButton>
                     </Tooltip>
-                </Hidden>
-            </Box>
-        </HeaderWrapper>
-        )
+                </RightButtonsWrapperLg>
+            </HeaderWrapperLg>
+        </Hidden>
+    )
 };
+
+export default Header;
+/* <HeaderUserbox /> */
--- platform/frontend/src/components/Dashboard/index.tsx
+++ platform/frontend/src/components/Dashboard/index.tsx
@@ -1,16 +1,30 @@
 import { FC, useState } from 'react';
+import { Box, Hidden } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import Sidebar from './Sidebar';
+import Header from './Header';
+
+const DashboardWrapper: FC = styled(Box)(
+    ({ theme }) => ({
+        width: '100%',
+        height: '100%',
+    })
+);
 
 const Dashboard: FC = () => {
-    const [sidebarToggled, setSidebarToggled] = useState(false);
+    const [sidebarToggled, setSidebarToggled] = useState<boolean>(false);
     const toggleSidebar = () => {
-        setSidebarToggled(sidebarToggled);
     };
     return (
-        <Sidebar
-            sidebarToggled={sidebarToggled}
-            toggleSidebar={toggleSidebar}
-        />
+        <Hidden lgDown>
+            <DashboardWrapper>
+                <Sidebar
+                    sidebarToggled={sidebarToggled}
+                    toggleSidebar={() => setSidebarToggled(!sidebarToggled)}
+                />
+                <Header />
+            </DashboardWrapper>
+        </Hidden>
     );
 };
 
