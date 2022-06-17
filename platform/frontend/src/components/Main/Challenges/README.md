acfa8b9c6898f0321aa1908509668561a344f1a5
zeze-zeze <zezectf@gmail.com>
2022-06-17 14:18:55 +0000

    feat: show challenge info, source code and connect to contract

---
+++ platform/frontend/src/components/Main/Challenges/README.md
@@ -0,0 +1,5 @@
+# Challenges
+## Usage
+1. 建立 chal 檔案放合約的原始碼
+2. 建立 info 檔案放合約的資訊，必須要有 address 與 abi
+3. 在 index.tsx 中引入上述兩個檔案，並使用 `setChal`、`setInfo` 設定題目
---
+++ platform/frontend/src/components/Main/Solving/index.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react';
+import MainWrapper from '..';
+
+/* 
+ * ../index.tsx 有提供一些已經 styled 好的一些 Wrapper 可以用。
+ * 例如如果要用 HeaderTypography 的話，可以用
+ * import { MainWrapper } from '..'
+ * 直接載入
+ */
+
+const Template: FC = () => {
+
+    return (
+        <MainWrapper title="template">
+            <h1>
+                CHANGE HERE!!!
+            </h1>
+        </MainWrapper>
+    );
+}
+
+/* 
+ * 之後記得在 Router/route.tsx 加入 route 喔！
+ */
+
+export default Template;
