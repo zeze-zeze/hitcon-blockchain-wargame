22df09c7d05247e04158c279805d2a0b4bafb746
AlbertLin0327 <linhsinkai@gmail.com>
2022-07-08 19:26:16 +0800

    feat: add chal2

---
+++ contracts/challenges/Chal2/Vuln2.sol
@@ -0,0 +1,52 @@
+//SPSPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+/// @title Vulnerability for challenge two
+/// @author Albert
+contract Vuln2 {
+    /*************************
+     **    MAIN VARIABLE     **
+     *************************/
+    mapping(address => bool) public crackerList;
+
+    /*************************
+     **    HELPER SECTION    **
+     *************************/
+    modifier winCondition() {
+        require(crackerList[tx.origin] == true);
+        _;
+    }
+
+    modifier contractGate() {
+        require(msg.sender != tx.origin);
+        _;
+    }
+
+    modifier gasGate() {
+        require((gasleft() % 22) == 0);
+        _;
+    }
+
+    modifier keyGate(bytes8 _gateKey) {
+        unchecked {
+            require(
+                uint64(bytes8(keccak256(abi.encodePacked(msg.sender)))) ^
+                    uint64(_gateKey) ==
+                    uint64(0) - 1
+            );
+        }
+        _;
+    }
+
+    /*************************
+     **     USERS SECTION    **
+     *************************/
+    function crack(bytes8 _gateKey)
+        public
+        contractGate
+        gasGate
+        keyGate(_gateKey)
+    {
+        crackerList[tx.origin] = true;
+    }
+}
