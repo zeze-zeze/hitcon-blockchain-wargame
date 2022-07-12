1151a8576dce57e9bd147544ea8b8d358fe54880
wxrdnx <wxrdnx@protonmail.com>
2022-07-12 22:31:08 +0800

    add missing files

---
+++ platform/backend/src/Routes.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+//import registerCallback from "./api/Register";
+import faucetCallBack from "./api/Faucet";
+import solveCallBack from "./api/Solve";
+
+const router = express.Router({ caseSensitive: true });
+
+//router.post("/register", registerCallback);
+router.post("/solve", solveCallBack);
+router.post("/faucet", faucetCallBack);
+
+export default router;
