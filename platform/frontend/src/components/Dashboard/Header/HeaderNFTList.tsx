9066a29891865a7c10f8bd53638ede71f97ab9af
wxrdnx <wxrdnx@protonmail.com>
2022-07-19 21:42:44 +0800

    Fixed several import and display errors

--- platform/frontend/src/components/Dashboard/Header/HeaderNFTList.tsx
+++ platform/frontend/src/components/Dashboard/Header/HeaderNFTList.tsx
@@ -1,245 +1,248 @@
-import { FC, useEffect, useState } from "react";
-import { Avatar, Grid, Snackbar, Alert } from "@mui/material";
+import { FC, useContext, useEffect, useState } from "react";
+import { Avatar, Grid, Snackbar, Alert, Tooltip } from "@mui/material";
 import LoadingButton from "@mui/lab/LoadingButton";
 import { useWeb3React } from "@web3-react/core";
 import SuspenseComponent from "../../Suspense";
-import Web3 from "web3/dist/web3.min.js";
+import Web3 from "web3";
+import { AbiItem } from "web3-utils";
 import axios from "axios";
 import info from "./info.json";
+import useSolvedProblems from "hooks/useSolvedProblems";
+import { useTheme } from "@mui/material/styles";
+import LanguageContext from "contexts/LanguageContext";
 
 const web3 = new Web3(Web3.givenProvider);
 
 const HeaderNFTList: FC = () => {
-  const { account, active } = useWeb3React();
-  const [chal1State, setChal1State] = useState(false);
-  const [chal2State, setChal2State] = useState(false);
-  const [chal3State, setChal3State] = useState(false);
-  const [chal4State, setChal4State] = useState(false);
-  const [chal5State, setChal5State] = useState(false);
-  const [chal6State, setChal6State] = useState(false);
+    const { account, active } = useWeb3React();
+    const [chal1State, setChal1State] = useState(false);
+    const [chal2State, setChal2State] = useState(false);
+    const [chal3State, setChal3State] = useState(false);
+    const [chal4State, setChal4State] = useState(false);
+    const [chal5State, setChal5State] = useState(false);
+    const [chal6State, setChal6State] = useState(false);
 
-  const [showLoading, setShowLoading] = useState(false);
-  const [showSnackBar, setShowSnackBar] = useState(0);
+    const [showLoading, setShowLoading] = useState(false);
+    const [showSnackBar, setShowSnackBar] = useState(0);
 
-  const chal1Contract = new web3.eth.Contract(
-    info["p1"]["abi"],
-    info["p1"]["addr"]
-  );
-  const chal2Contract = new web3.eth.Contract(
-    info["p2"]["abi"],
-    info["p2"]["addr"]
-  );
-  const chal3Contract = new web3.eth.Contract(
-    info["p3"]["abi"],
-    info["p3"]["addr"]
-  );
-  const chal4Contract = new web3.eth.Contract(
-    info["p4"]["abi"],
-    info["p4"]["addr"]
-  );
-  const chal5Contract = new web3.eth.Contract(
-    info["p5"]["abi"],
-    info["p5"]["addr"]
-  );
-  const chal6Contract = new web3.eth.Contract(
-    info["p5"]["abi"],
-    info["p5"]["addr"]
-  );
+    const { getSolvedProblems } = useSolvedProblems();
+    const solvedProblems = getSolvedProblems();
+    const theme = useTheme();
+    const { multiLang } = useContext(LanguageContext);
 
-  const requestNFT = async () => {
-    setShowLoading(true);
-    await axios
-      .post(process.env.REACT_APP_BASE_API_URL + "/api/hitcon-nft-sender", {
-        address: account,
-      })
-      .then((response) => {
-        // console.log(response);
-        setShowSnackBar(1);
-      })
-      .catch((error) => {
-        // console.log(error);
-        setShowSnackBar(2);
-      });
-    setShowLoading(false);
-  };
+    const nftImgLinks = [
+        "https://i.imgur.com/fSFl7io.png",
+        "https://i.imgur.com/OP9jv73.png",
+        "https://i.imgur.com/VRYrAZ5.png",
+        "https://i.imgur.com/IFRMdTA.png",
+        "https://i.imgur.com/lgGrqE6.png",
+        "https://i.imgur.com/yvn5IdB.png"
+    ];
 
-  const handleClose = () => {
-    setShowSnackBar(0);
-  };
+    const chal1Contract = new web3.eth.Contract(
+        info["p1"]["abi"] as AbiItem[],
+        info["p1"]["addr"]
+    );
+    const chal2Contract = new web3.eth.Contract(
+        info["p2"]["abi"] as AbiItem[],
+        info["p2"]["addr"]
+    );
+    const chal3Contract = new web3.eth.Contract(
+        info["p3"]["abi"] as AbiItem[],
+        info["p3"]["addr"]
+    );
+    const chal4Contract = new web3.eth.Contract(
+        info["p4"]["abi"] as AbiItem[],
+        info["p4"]["addr"]
+    );
+    const chal5Contract = new web3.eth.Contract(
+        info["p5"]["abi"] as AbiItem[],
+        info["p5"]["addr"]
+    );
+    const chal6Contract = new web3.eth.Contract(
+        info["p5"]["abi"] as AbiItem[],
+        info["p5"]["addr"]
+    );
 
-  useEffect(() => {
-    if (account !== undefined) {
-      chal1Contract.methods
-        .addressToSolved(account)
-        .call({ from: account })
-        .then((t: boolean) => setChal1State(t));
-      chal2Contract.methods
-        .addressToSolved(account)
-        .call({ from: account })
-        .then((t: boolean) => setChal2State(t));
-      chal3Contract.methods
-        .addressToSolved(account)
-        .call({ from: account })
-        .then((t: boolean) => setChal3State(t));
-      chal4Contract.methods
-        .addressToSolved(account)
-        .call({ from: account })
-        .then((t: boolean) => setChal4State(t));
-      chal5Contract.methods
-        .addressToSolved(account)
-        .call({ from: account })
-        .then((t: boolean) => setChal5State(t));
-      chal6Contract.methods
-        .addressToSolved(account)
-        .call({ from: account })
-        .then((t: boolean) => setChal6State(t));
-    }
-  }, [account]);
+    const requestNFT = async () => {
+        setShowLoading(true);
+        await axios
+            .post(process.env.REACT_APP_BASE_API_URL + "/api/hitcon-nft-sender", {
+                address: account,
+            })
+            .then((response) => {
+                // console.log(response);
+                setShowSnackBar(1);
+            })
+            .catch((error) => {
+                // console.log(error);
+                setShowSnackBar(2);
+            });
+        setShowLoading(false);
+    };
 
-  chal1Contract.events
-    .hadSolved({ filter: { _solver: account } })
-    .on("data", (log: any) => {
-      setChal1State(true);
-    });
+    const handleClose = () => {
+        setShowSnackBar(0);
+    };
 
-  chal2Contract.events
-    .hadSolved({ filter: { _solver: account } })
-    .on("data", (log: any) => {
-      setChal2State(true);
-    });
+    useEffect(() => {
+        if (account !== undefined) {
+            chal1Contract.methods
+                .addressToSolved(account)
+                .call({ from: account })
+                .then((t: boolean) => setChal1State(t));
+            chal2Contract.methods
+                .addressToSolved(account)
+                .call({ from: account })
+                .then((t: boolean) => setChal2State(t));
+            chal3Contract.methods
+                .addressToSolved(account)
+                .call({ from: account })
+                .then((t: boolean) => setChal3State(t));
+            chal4Contract.methods
+                .addressToSolved(account)
+                .call({ from: account })
+                .then((t: boolean) => setChal4State(t));
+            chal5Contract.methods
+                .addressToSolved(account)
+                .call({ from: account })
+                .then((t: boolean) => setChal5State(t));
+            chal6Contract.methods
+                .addressToSolved(account)
+                .call({ from: account })
+                .then((t: boolean) => setChal6State(t));
+        }
+    }, [account]);
 
-  chal3Contract.events
-    .hadSolved({ filter: { _solver: account } })
-    .on("data", (log: any) => {
-      setChal3State(true);
-    });
-  chal4Contract.events
-    .hadSolved({ filter: { _solver: account } })
-    .on("data", (log: any) => {
-      setChal4State(true);
-    });
-  chal5Contract.events
-    .hadSolved({ filter: { _solver: account } })
-    .on("data", (log: any) => {
-      setChal5State(true);
-    });
-  chal6Contract.events
-    .hadSolved({ filter: { _solver: account } })
-    .on("data", (log: any) => {
-      setChal6State(true);
-    });
+    chal1Contract.events
+        .hadSolved({ filter: { _solver: account } })
+        .on("data", (log: any) => {
+            setChal1State(true);
+        });
 
-  chal1Contract.events
-    .newSolved({ filter: { _solver: account } })
-    .on("data", (log: any) => {
-      setChal1State(true);
-    });
+    chal2Contract.events
+        .hadSolved({ filter: { _solver: account } })
+        .on("data", (log: any) => {
+            setChal2State(true);
+        });
 
-  chal2Contract.events
-    .newSolved({ filter: { _solver: account } })
-    .on("data", (log: any) => {
-      setChal2State(true);
-    });
+    chal3Contract.events
+        .hadSolved({ filter: { _solver: account } })
+        .on("data", (log: any) => {
+            setChal3State(true);
+        });
+    chal4Contract.events
+        .hadSolved({ filter: { _solver: account } })
+        .on("data", (log: any) => {
+            setChal4State(true);
+        });
+    chal5Contract.events
+        .hadSolved({ filter: { _solver: account } })
+        .on("data", (log: any) => {
+            setChal5State(true);
+        });
+    chal6Contract.events
+        .hadSolved({ filter: { _solver: account } })
+        .on("data", (log: any) => {
+            setChal6State(true);
+        });
 
-  chal3Contract.events
-    .newSolved({ filter: { _solver: account } })
-    .on("data", (log: any) => {
-      setChal3State(true);
-    });
-  chal4Contract.events
-    .newSolved({ filter: { _solver: account } })
-    .on("data", (log: any) => {
-      setChal4State(true);
-    });
-  chal5Contract.events
-    .newSolved({ filter: { _solver: account } })
-    .on("data", (log: any) => {
-      setChal5State(true);
-    });
-  chal6Contract.events
-    .newSolved({ filter: { _solver: account } })
-    .on("data", (log: any) => {
-      setChal6State(true);
-    });
+    chal1Contract.events
+        .newSolved({ filter: { _solver: account } })
+        .on("data", (log: any) => {
+            setChal1State(true);
+        });
 
-  const nftInfo = [
-    {
-      url: "https://i.imgur.com/nHHO5Q8.jpg",
-      gain: chal1State,
-    },
-    {
-      url: "https://i.imgur.com/nHHO5Q8.jpg",
-      gain: chal2State,
-    },
-    {
-      url: "https://i.imgur.com/nHHO5Q8.jpg",
-      gain: chal3State,
-    },
-    {
-      url: "https://i.imgur.com/nHHO5Q8.jpg",
-      gain: chal4State,
-    },
-    {
-      url: "https://i.imgur.com/nHHO5Q8.jpg",
-      gain: chal5State,
-    },
-    {
-      url: "https://i.imgur.com/nHHO5Q8.jpg",
-      gain: chal6State,
-    },
-  ];
+    chal2Contract.events
+        .newSolved({ filter: { _solver: account } })
+        .on("data", (log: any) => {
+            setChal2State(true);
+        });
 
-  return (
-    <>
-      <Snackbar
-        open={showSnackBar != 0}
-        autoHideDuration={6000}
-        onClose={handleClose}
-      >
-        <Alert
-          onClose={handleClose}
-          severity={showSnackBar === 1 ? "success" : "error"}
-          sx={{ width: "100%" }}
-        >
-          {showSnackBar === 1 ? "Request NFT Success" : "ERROR! Request failed"}
-        </Alert>
-      </Snackbar>
-      <Grid container justifyContent="center" alignItems="center" spacing={8}>
-        {nftInfo.map(({ url, gain }, id) => (
-          <Grid item xs={1} key={id}>
-            <Avatar
-              variant="rounded"
-              src={url}
-              sx={{
-                opacity: gain ? "100%" : "40%",
-              }}
-            />
-          </Grid>
-        ))}
+    chal3Contract.events
+        .newSolved({ filter: { _solver: account } })
+        .on("data", (log: any) => {
+            setChal3State(true);
+        });
+    chal4Contract.events
+        .newSolved({ filter: { _solver: account } })
+        .on("data", (log: any) => {
+            setChal4State(true);
+        });
+    chal5Contract.events
+        .newSolved({ filter: { _solver: account } })
+        .on("data", (log: any) => {
+            setChal5State(true);
+        });
+    chal6Contract.events
+        .newSolved({ filter: { _solver: account } })
+        .on("data", (log: any) => {
+            setChal6State(true);
+        });
 
-        {chal1State &&
-          chal2State &&
-          chal3State &&
-          chal4State &&
-          chal5State &&
-          chal6State && (
-            <>
-              <Grid item lg={3} key={7}>
-                <LoadingButton
-                  variant="contained"
-                  onClick={requestNFT}
-                  loading={showLoading}
-                  loadingIndicator="Requesting..."
+    return (
+        <>
+            <Snackbar
+                open={showSnackBar != 0}
+                autoHideDuration={6000}
+                onClose={handleClose}
+            >
+                <Alert
+                    onClose={handleClose}
+                    severity={showSnackBar === 1 ? "success" : "error"}
+                    sx={{ width: "100%" }}
                 >
-                  Request NFT
-                </LoadingButton>
-              </Grid>
-            </>
-          )}
-      </Grid>
-    </>
-  );
-  return <></>;
+                    {showSnackBar === 1 ? "Request NFT Success" : "ERROR! Request failed"}
+                </Alert>
+            </Snackbar>
+            <Grid container justifyContent="center" alignItems="center" spacing={8}>
+                {
+                    solvedProblems.map((solved: boolean, idx: number) => (
+                        <Grid item xs={1} key={idx}>
+                            <Tooltip arrow title={multiLang?.problems.challenges[idx].title}>
+                                <Avatar
+                                    variant="rounded"
+                                    src={nftImgLinks[idx]}
+                                    sx={{
+                                        borderWidth: "3px",
+                                        borderStyle: "dashed",
+                                        borderColor: solved ? theme.colors.success.main : theme.colors.error.main,
+                                        padding: "1px"
+                                    }}
+                                    imgProps={{
+                                        style: {
+                                            opacity: solved ? "100%" : "40%",
+                                        }
+                                    }}
+                                />
+                            </Tooltip>
+                        </Grid>
+                    ))
+                }
+
+                {chal1State &&
+                    chal2State &&
+                    chal3State &&
+                    chal4State &&
+                    chal5State &&
+                    chal6State && (
+                        <>
+                            <Grid item lg={3} key={7}>
+                                <LoadingButton
+                                    variant="contained"
+                                    onClick={requestNFT}
+                                    loading={showLoading}
+                                    loadingIndicator="Requesting..."
+                                >
+                                    Request NFT
+                                </LoadingButton>
+                            </Grid>
+                        </>
+                    )}
+            </Grid>
+        </>
+    );
+    return <></>;
 };
 
 export default HeaderNFTList;
--- platform/frontend/src/components/Dashboard/Header/index.tsx
+++ platform/frontend/src/components/Dashboard/Header/index.tsx
@@ -1,12 +1,10 @@
 import { FC, useContext } from "react";
 import {
-  Box,
-  Avatar,
-  Button,
-  Grid,
-  IconButton,
-  Tooltip,
-  useMediaQuery,
+    Box,
+    IconButton,
+    Tooltip,
+    useMediaQuery,
+    lighten
 } from "@mui/material";
 import { styled, useTheme } from "@mui/material/styles";
 import MenuIcon from "@mui/icons-material/Menu";
@@ -15,23 +13,25 @@ import HeaderButtons from "./HeaderButtons";
 import HeaderUserMenu from "./HeaderUserMenu";
 import SidebarToggledContext from "contexts/SidebarToggledContext";
 import { useWeb3React } from "@web3-react/core";
-import LoginButton from "./LoginButton";
 import HeaderNFTList from "./HeaderNFTList";
+import useSolvedProblems from "hooks/useSolvedProblems";
+import LanguageContext from "contexts/LanguageContext";
+import ConnectButton from "components/Connect/ConnectButton";
 
-const HeaderWrapper: FC = styled(Box)(({ theme }) => ({
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-  position: "fixed",
-  top: 0,
-  left: 0,
-  right: 0,
-  width: "auto",
-  height: theme.header.height,
-  color: theme.header.textColor,
-  padding: theme.spacing(3),
-  backgroundColor: theme.header.background,
-  boxShadow: theme.header.boxShadow,
+const HeaderWrapper = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    width: 'auto',
+    height: theme.header.height,
+    color: theme.header.textColor,
+    padding: theme.spacing(3),
+    backgroundColor: theme.colors.alpha.black[100],
+    boxShadow: lighten(theme.colors.primary.main, 0.7),
 }));
 
 const HeaderComponentsWrapper = styled(Box)(
@@ -43,19 +43,7 @@ const HeaderComponentsWrapper = styled(Box)(
     })
 );
 
-const HeaderComponentsWrapper: FC = styled(Box)(({ theme }) => ({
-  display: "flex",
-  flexWrap: "nowrap",
-  justifyContent: "space-around",
-  alignItems: "center",
-}));
-
 const Header: FC = () => {
-  const theme = useTheme();
-  const lgUp = useMediaQuery(theme.breakpoints.up("lg"));
-  const { sidebarToggled, toggleSidebar } = useContext(SidebarToggledContext);
-  const { active } = useWeb3React();
-
     const theme = useTheme();
     const lgUp = useMediaQuery(theme.breakpoints.up("lg"));
     const { sidebarToggled, toggleSidebar } = useContext(SidebarToggledContext);
@@ -65,15 +53,6 @@ const Header: FC = () => {
     const solvedProblems = getSolvedProblems();
     const { multiLang } = useContext(LanguageContext);
 
-    const nftImgLinks = [
-        "https://i.imgur.com/fSFl7io.png",
-        "https://i.imgur.com/OP9jv73.png",
-        "https://i.imgur.com/VRYrAZ5.png",
-        "https://i.imgur.com/IFRMdTA.png",
-        "https://i.imgur.com/lgGrqE6.png", 
-        "https://i.imgur.com/yvn5IdB.png"
-    ];
-
     /* 
      * Adjust the width & left property according to
      * 1. current screen size
@@ -82,60 +61,35 @@ const Header: FC = () => {
 
     return (
         <>
-        {
-            /* 
-             * If the current window size is big enough & left sidebar is toggled,
-             * the header should not span across the menu.
-             * Otherwise, the header"s width should be equivalent to the screen"s width
-             */
-            <HeaderWrapper sx={{ left: lgUp && sidebarToggled ? theme.sidebar.width: 0 }}>
-                <HeaderComponentsWrapper>
-                    <Tooltip arrow title={multiLang?.dashboard.header.tooltip.toggleMenu}>
-                        <IconButton color="primary" onClick={toggleSidebar}>
-                            {sidebarToggled ? <MenuOpenIcon /> : <MenuIcon />}
-                        </IconButton>
-                    </Tooltip>
-                </HeaderComponentsWrapper>
-                <HeaderComponentsWrapper>
-                    <HeaderNFTList />
-                    <Grid container justifyContent="center" alignItems="center" spacing={7}>
-                    {
-                        solvedProblems.map((solved: boolean, idx: number) => (
-                            <Grid item xs={1} key={idx}>
-                                <Tooltip arrow title={multiLang?.problems.challenges[idx].title}>
-                                    <Avatar
-                                        variant="rounded"
-                                        src={nftImgLinks[idx]}
-                                        sx={{
-                                            borderWidth: "3px",
-                                            borderStyle: "dashed",
-                                            borderColor: solved ? theme.colors.success.main : theme.colors.error.main,
-                                            padding: "1px"
-                                        }}
-                                        imgProps={{
-                                            style: {
-                                                opacity: solved ? "100%" : "40%",
-                                            }
-                                        }}
-                                    />
-                                </Tooltip>
-                            </Grid>
-                        ))
-                    }
-                    </Grid>
-                </HeaderComponentsWrapper>
-                <HeaderComponentsWrapper>
-                    <HeaderButtons />
-                    {
-                        active ? (
-                            <HeaderUserMenu />
-                        ) : (
-                           <ConnectButton />
-                        )
-                    }
-                </HeaderComponentsWrapper>
-            </HeaderWrapper>
-        }
+            {
+                /* 
+                 * If the current window size is big enough & left sidebar is toggled,
+                 * the header should not span across the menu.
+                 * Otherwise, the header"s width should be equivalent to the screen"s width
+                 */
+                <HeaderWrapper sx={{ left: lgUp && sidebarToggled ? theme.sidebar.width : 0 }}>
+                    <HeaderComponentsWrapper>
+                        <Tooltip arrow title={multiLang?.dashboard.header.tooltip.toggleMenu}>
+                            <IconButton color="primary" onClick={toggleSidebar}>
+                                {sidebarToggled ? <MenuOpenIcon /> : <MenuIcon />}
+                            </IconButton>
+                        </Tooltip>
+                    </HeaderComponentsWrapper>
+                    <HeaderComponentsWrapper>
+                        <HeaderNFTList />
+                    </HeaderComponentsWrapper>
+                    <HeaderComponentsWrapper>
+                        <HeaderButtons />
+                        {
+                            active ? (
+                                <HeaderUserMenu />
+                            ) : (
+                                <ConnectButton />
+                            )
+                        }
+                    </HeaderComponentsWrapper>
+                </HeaderWrapper>
+            }
         </>
     )
 };
