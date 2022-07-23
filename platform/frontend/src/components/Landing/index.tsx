031e3d2a12382005ba43a00a159f8e316870ec0b
wxrdnx <wxrdnx@protonmail.com>
2022-07-24 00:21:10 +0800

    resolve problem id conflict

--- platform/frontend/src/components/Landing/index.tsx
+++ platform/frontend/src/components/Landing/index.tsx
@@ -1,14 +1,20 @@
-import { FC, useState, useCallback, useContext } from 'react';
+import { ChangeEvent, FC, useCallback, useContext, useState } from 'react';
 import {
     Box,
     Button,
     Card,
     Container,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
     FormControl,
     Grid,
     InputLabel,
     MenuItem,
     Select,
+    TextField,
     Typography,
     useMediaQuery
 } from '@mui/material';
@@ -17,6 +23,9 @@ import { SelectChangeEvent } from '@mui/material/Select';
 import { HelmetProvider, Helmet } from 'react-helmet-async';
 import { styled, useTheme } from '@mui/material/styles';
 import LanguageContext from 'contexts/LanguageContext';
+import WaitEffect from 'components/WaitEffect';
+import WaitEffectContext from "contexts/WaitEffectContext";
+import axios from "axios";
 
 const LandingWrapper = styled(Container)(
     ({ theme }) => ({
@@ -59,16 +68,77 @@ const LandingButtonsWrapper = styled(Box)(
 );
 
 const Landing: FC = () => {
+
+    const [open, setOpen] = useState<boolean>(false);
+    const [token, setToken] = useState<string>("");
     const { lang, changeLang, multiLang } = useContext(LanguageContext);
     const theme = useTheme();
     const minWidthUp = useMediaQuery("(min-width:1100px)");
     const navigate = useNavigate();
-    
+    const { setShowBackDrop, setShowSnackBar, setErrorMessage, setSuccessMessage } = useContext(WaitEffectContext);
+
+    const verifyJWT = useCallback(async () => {
+        setOpen(false);
+        setShowBackDrop(true);
+        try {
+            await axios
+            .post(process.env.REACT_APP_BASE_API_URL + "/login", {
+                token: token,
+            }, {
+                withCredentials: true
+            });
+            setSuccessMessage(multiLang?.success.login);
+            setShowSnackBar(1);
+            setShowBackDrop(false);
+            navigate("/home");
+            
+        } catch (error) {
+            setErrorMessage(multiLang?.error.login);
+            setShowSnackBar(2);
+            setShowBackDrop(false);
+        }
+    }, [token]);
+
     return (
         <HelmetProvider>
             <Helmet>
                 <title>Hitcon Wargame</title>
             </Helmet>
+            <WaitEffect />
+            <Dialog
+                open={open}
+                onClose={() => setOpen(false)}
+            >
+                <DialogTitle id="dialog-title">
+                    {multiLang?.landing.dialog.title}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="dialog-content">
+                        {multiLang?.landing.dialog.content}
+                    </DialogContentText>
+                    <Box>
+                        <TextField
+                            required
+                            id="jwt"
+                            placeholder="eyJhbGci..."
+                            value={token}
+                            onChange={(event: ChangeEvent<HTMLInputElement>) => setToken(event.target.value)}
+                            fullWidth
+                            sx={{
+                                mt: theme.spacing(2)
+                            }}
+                        />
+                    </Box>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setOpen(false)}>
+                        {multiLang?.landing.dialog.buttons.cancel}
+                    </Button>
+                    <Button onClick={verifyJWT}>
+                        {multiLang?.landing.dialog.buttons.submit}
+                    </Button>
+                </DialogActions>
+            </Dialog>
             <LandingWrapper sx={{ width: minWidthUp ? '50%' : '550px' }}>
                 <LandingCard>
                     <Grid spacing={10} container>
@@ -101,31 +171,27 @@ const Landing: FC = () => {
                                 </Select>
                             </FormControl>
                             <LandingButtonsWrapper>
-                                <Grid item xs={6} mx="auto">
-                                    <Button
-                                        variant="contained"
-                                        size="large"
-                                        onClick={() => {
-                                            window.location.href = 'https://hitcon.org/2022/';
-                                        }}
-                                        sx={{ margin: theme.spacing(3) }}
-                                    >
-                                        {multiLang?.landing.buttons[0].text}
-                                    </Button>
-                                </Grid>
-                                <Grid item xs={6} mx="auto">
-                                    <Button
-                                        color="error"
-                                        variant="contained"
-                                        size="large"
-                                        onClick={() => {
-                                            navigate('/home');
-                                        }}
-                                        sx={{ margin: theme.spacing(3) }}
-                                    >
-                                        {multiLang?.landing.buttons[1].text}
-                                    </Button>
-                                </Grid>
+                                <Button
+                                    variant="contained"
+                                    size="large"
+                                    onClick={() => {
+                                        setOpen(true);
+                                    }}
+                                    sx={{ margin: theme.spacing(3) }}
+                                >
+                                    {multiLang?.landing.buttons[0].text}
+                                </Button>
+                                <Button
+                                    color="error"
+                                    variant="contained"
+                                    size="large"
+                                    onClick={() => {
+                                        navigate('/home');
+                                    }}
+                                    sx={{ margin: theme.spacing(3) }}
+                                >
+                                    {multiLang?.landing.buttons[1].text}
+                                </Button>
                             </LandingButtonsWrapper>
                         </Grid>
                     </Grid>
