ed8b2e0fe504d5f887a01d7c769abed2dc43e09a
zeze-zeze <zezectf@gmail.com>
2022-07-23 02:23:00 +0000

    feat: add some checking before sending NFT

--- platform/backend/src/api/HitconNFTSender.ts
+++ platform/backend/src/api/HitconNFTSender.ts
@@ -3,6 +3,7 @@ import createError from "http-errors";
 import asyncHandler from "express-async-handler";
 import { checkAddress } from "../web3/utils";
 import { web3 } from "../web3/index";
+import { AbiItem } from "web3-utils";
 import config from "../config";
 
 const { BadRequest, UnprocessableEntity } = createError;
@@ -15,6 +16,15 @@ const send = async (address: string) => {
     hitconNFTSenderABI["abi"],
     hitconNFTSenderABI["address"]
   );
+  
+  // Check whether the address had got NFT
+  const hadSent = await hitconNFTSenderContract.methods.balanceOf(address, 1).call({from: address});
+  console.log(hadSent);
+  if (hadSent !== 0) {
+    return {"status": "fail", "msg": "NFT can only request 1."};
+  }
+
+  // Send NFT
   const transaction = hitconNFTSenderContract.methods.allSolved(address);
 
   const options = {
@@ -30,7 +40,7 @@ const send = async (address: string) => {
   const receipt = await web3.eth.sendSignedTransaction(
     String(signed.rawTransaction)
   );
-  return receipt;
+  return {"status": "success", "msg": "NFT sent.", "receipt": receipt};
 };
 
 const hitconNFTSenderCallBack = asyncHandler(async (req: Request, res: Response, next: NextFunction): Promise<any> => {
@@ -44,7 +54,58 @@ const hitconNFTSenderCallBack = asyncHandler(async (req: Request, res: Response,
     return next(new UnprocessableEntity("Incorrect Wallet Address."));
   }
 
-  await send(address);
+  // Check all challenges solved
+  const info = JSON.parse(
+    JSON.stringify(require("../../../frontend/src/hooks/info.json"))
+  );
+
+  const chal1Contract = new web3.eth.Contract(
+    info["p1"]["abi"] as AbiItem[],
+    info["p1"]["addr"]
+  );
+  const chal2Contract = new web3.eth.Contract(
+    info["p2"]["abi"] as AbiItem[],
+    info["p2"]["addr"]
+  );
+  const chal3Contract = new web3.eth.Contract(
+    info["p3"]["abi"] as AbiItem[],
+    info["p3"]["addr"]
+  );
+  const chal4Contract = new web3.eth.Contract(
+    info["p4"]["abi"] as AbiItem[],
+    info["p4"]["addr"]
+  );
+  const chal5Contract = new web3.eth.Contract(
+    info["p5"]["abi"] as AbiItem[],
+    info["p5"]["addr"]
+  );
+  const chal6Contract = new web3.eth.Contract(
+    info["p5"]["abi"] as AbiItem[],
+    info["p5"]["addr"]
+  );
+
+  let allSolved = true;
+  const contracts = [chal1Contract, chal2Contract, chal3Contract, chal4Contract, chal5Contract, chal6Contract];
+  await Promise.all(contracts.map(async (contract) => {
+    const solved = await contract.methods
+        .addressToSolved(address)
+        .call({ from: address })
+    if (!solved) {
+      allSolved = false;
+    }
+  }));
+
+  if (!allSolved) {
+    return next(new UnprocessableEntity("Challenges are not all solved."));
+  }
+
+  // TODO: Check JWT token
+
+  // Send NFT
+  const nft = await send(address);
+  if (nft["status"]) {
+    return next(new UnprocessableEntity(nft["msg"]));
+  }
 
   if (res.statusCode === 500) {
     return res.status(500);
