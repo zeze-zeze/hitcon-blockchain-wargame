5eb9deed707d85273907c876772c6acfa7c1d8e0
AlbertLin0327 <linhsinkai@gmail.com>
2022-08-10 10:17:00 +0800

    remove token set

--- platform/backend/src/api/HitconNFTSender.ts
+++ platform/backend/src/api/HitconNFTSender.ts
@@ -21,7 +21,7 @@ const send = async (address: string) => {
   const hadSent = await hitconNFTSenderContract.methods
     .balanceOf(address, 1)
     .call({ from: config.PublicKey });
-
+  g;
   if (hadSent !== "0") {
     return { status: "fail", msg: "NFT can only be requested once" };
   }
