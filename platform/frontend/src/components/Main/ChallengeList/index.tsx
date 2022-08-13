ec0ec89c21e01a60a9fd5a45e1592ba2730e8e4f
wxrdnx <wxrdnx@protonmail.com>
2022-08-13 17:56:40 +0800

    Add title to challenges and improve challenge cards

--- platform/frontend/src/components/Main/ChallengeList/index.tsx
+++ platform/frontend/src/components/Main/ChallengeList/index.tsx
@@ -1,10 +1,11 @@
 import { FC, useContext, useRef } from 'react';
 import { Grid } from '@mui/material';
-import MainWrapper from 'components/Main';
-
+import MainWrapper, { HeaderTypography, HeaderWrapper, SubtitleTypography } from 'components/Main';
 import ChallengeCard from './ChallengeCard';
 import LanguageContext from 'contexts/LanguageContext';
 import Web3Context from 'contexts/Web3Context';
+import { Container } from '@mui/system';
+import { useTheme } from "@mui/material/styles";
 
 type ChallengeType = {
     title: string;
@@ -13,12 +14,23 @@ type ChallengeType = {
 };
 
 const ChallengeList: FC = () => {
+    const theme = useTheme();
     const { solved } = useContext(Web3Context);
     const { multiLang } = useContext(LanguageContext);
     const challengesRef = useRef<ChallengeType[]>(multiLang?.challenges.list);
 
     return (
         <MainWrapper title="Challenges">
+            <Grid item xs={12}>
+                <HeaderWrapper>
+                    <HeaderTypography>
+                        {multiLang?.challenges.title}
+                    </HeaderTypography>
+                    <SubtitleTypography>
+                        {multiLang?.challenges.subtitle}
+                    </SubtitleTypography>
+                </HeaderWrapper>
+            </Grid>
             <Grid container justifyContent="center">
                 {
                     challengesRef.current &&
