56fda74320f6cc2603421871f87f55a7ed8a4767
wxrdnx <wxrdnx@protonmail.com>
2022-08-13 15:49:09 +0800

    Improve error message

--- platform/backend/utils/sign.js
+++ platform/backend/utils/sign.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const secret = "secret";
 
 const result = jwt.sign({
-    "scope": "wargame wargame_premium"
+    "scope": "wargame wargame_premiuma"
 }, secret, {
     issuer: "https://hitcon.org",
     subject: "bee41cf52c6c394399c6023328b812b8edf603f9a7e44788b752439375403c34",
